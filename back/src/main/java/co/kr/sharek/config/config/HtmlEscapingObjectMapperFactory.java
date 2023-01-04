f8fee741ae696aef59bbe6c48ccde02e8110417d
kimseongmin0301 <hippo159753@gmail.com>
2023-01-04 15:16:25 +0900

    XSS Filter 이모지이슈 해결

--- back/src/main/java/co/kr/sharek/config/config/HtmlEscapingObjectMapperFactory.java
+++ back/src/main/java/co/kr/sharek/config/config/HtmlEscapingObjectMapperFactory.java
@@ -30,13 +30,10 @@ public class HtmlEscapingObjectMapperFactory implements FactoryBean<ObjectMapper
         return ObjectMapper.class;
     }
 
-
-
     @Override
     public boolean isSingleton() {
         return true;
     }
-
 
     /**
      * XSS filter
@@ -65,7 +62,15 @@ public class HtmlEscapingObjectMapperFactory implements FactoryBean<ObjectMapper
         // and this for others; we don't need anything special here
         @Override
         public SerializableString getEscapeSequence(int ch) {
-            return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+            char charAt = (char) ch;
+            if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("\\u");
+                sb.append(String.format("%04x", ch));
+                return new SerializedString(sb.toString());
+            } else {
+                return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+            }
         }
     }
 }
