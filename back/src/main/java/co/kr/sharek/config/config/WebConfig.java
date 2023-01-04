08ed16e12c997eaa05a06059bcec05b155d3dc17
kimseongmin0301 <hippo159753@gmail.com>
2023-01-04 15:02:54 +0900

    XSS Filter 적용

--- back/src/main/java/co/kr/sharek/config/config/WebConfig.java
+++ back/src/main/java/co/kr/sharek/config/config/WebConfig.java
@@ -1,5 +1,8 @@
 package co.kr.sharek.config.config;
 
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +13,21 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOriginPatterns("*")
+            
         .allowCredentials(true)
         .allowedMethods("*");
+  }
+
+  /**
+   * XSS Filter
+   * @return
+   */
+  @Bean
+  public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+    FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
+    registrationBean.setFilter(new XssEscapeServletFilter());
+    registrationBean.setOrder(1);
+    registrationBean.addUrlPatterns("/*");
+    return registrationBean;
   }
 }
