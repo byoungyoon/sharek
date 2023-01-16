1aa67b225210d1545fce211e3eb08a6647177653
byoungyoon <bur5698@naver.com>
2023-01-16 13:57:24 +0900

    :green_heart: fix - useMemo 사용
    1. 계산식에 대해서 재사용을 위해 useMemo 사용

--- front/src/components/atoms/input/Input.tsx
+++ front/src/components/atoms/input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ForwardedRef, forwardRef } from 'react';
+import React, { CSSProperties, ForwardedRef, forwardRef, useMemo } from 'react';
 
 interface InputProps {
   theme?: 'gray' | 'orange';
@@ -8,7 +8,7 @@ interface InputProps {
 export const Input = forwardRef((props: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
   const { theme = 'gray', ...prop } = props;
 
-  const borderColor = theme === 'gray' ? 'border-myGray' : 'border-myOrange';
+  const borderColor = useMemo(() => (theme === 'gray' ? 'border-myGray' : 'border-myOrange'), [theme]);
   const classArr = ['border-2', `${borderColor}`, 'focus:outline-none', 'w-full', 'p-1.5', 'rounded-md'];
 
   return <input ref={ref} type="text" className={classArr.join(' ')} {...prop} />;
