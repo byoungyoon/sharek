ff6973e478f8d49255f1dbb06f54ac8f63d1e079
byoungyoon <bur5698@naver.com>
2023-01-16 13:53:53 +0900

    :green_heart: fix - Input, SelectInput 사용할 ref 연결
    1. Input => forwardRef를 이용하여 ref도 상속
    2. SearchInput => KeyDown Handler 추가하여 void 형으로 상속

--- front/src/components/atoms/input/Input.tsx
+++ front/src/components/atoms/input/Input.tsx
@@ -1,13 +1,15 @@
-import React, { ChangeEvent, CSSProperties } from 'react';
+import React, { CSSProperties, ForwardedRef, forwardRef } from 'react';
 
 interface InputProps {
   theme?: 'gray' | 'orange';
   style?: CSSProperties;
 }
 
-export const Input = ({ theme = 'gray', ...prop }: InputProps) => {
+export const Input = forwardRef((props: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
+  const { theme = 'gray', ...prop } = props;
+
   const borderColor = theme === 'gray' ? 'border-myGray' : 'border-myOrange';
   const classArr = ['border-2', `${borderColor}`, 'focus:outline-none', 'w-full', 'p-1.5', 'rounded-md'];
 
-  return <input type="text" className={classArr.join(' ')} {...prop} />;
-};
+  return <input ref={ref} type="text" className={classArr.join(' ')} {...prop} />;
+});
--- front/src/components/molecules/SearchInput.tsx
+++ front/src/components/molecules/SearchInput.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import Search from '@constants/svg/search.svg';
 import { Input } from '@components/atoms';
 
 interface SearchInputProps {
   theme?: 'gray' | 'orange';
+  onKeyPress: (value: string) => void;
 }
 
-export const SearchInput = ({ theme = 'gray' }: SearchInputProps) => {
-  const borderColor = theme === 'gray' ? 'fill-myGray' : 'fill-myOrange';
+export const SearchInput = (props: SearchInputProps) => {
+  const { theme = 'gray', onKeyPress, ...prop } = props;
+
+  const borderColor = useMemo(() => (theme === 'gray' ? 'fill-myGray' : 'fill-myOrange'), [theme]);
+  const target = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    target.current?.addEventListener('keydown', onKeyDown);
+  }, []);
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      target.current && onKeyPress(target.current?.value);
+    }
+  };
 
   return (
-    <div className="relative tablet:w-full laptop:w-lg desktop:w-lg ">
+    <div className="relative tablet:w-full laptop:w-lg desktop:w-lg">
       <div className="absolute top-2 right-2">
         <Search className={borderColor} width="24" height="24" />
       </div>
-      <Input theme={theme} style={{ paddingRight: '2.3rem' }} />
+      <Input ref={target} theme={theme} style={{ paddingRight: '2.3rem' }} />
     </div>
   );
 };
