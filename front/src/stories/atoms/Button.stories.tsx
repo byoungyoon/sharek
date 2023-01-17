8fe958073e8a080639853d9e0afd97f3a40985d8
byoungyoon <bur5698@naver.com>
2023-01-17 12:19:30 +0900

    :green_heart: fix - storybook 폴더 구조 세부화

--- front/src/stories/atoms/Button.stories.tsx
+++ front/src/stories/atoms/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Button } from '../components/atoms';
+import { Button } from '@components/atoms';
 
 export default {
   title: 'atoms/Button',
