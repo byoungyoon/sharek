5b0cda7baece1963570f3fd380f26c5b7ad5e692
byoungyoon <bur5698@naver.com>
2023-01-18 11:20:38 +0900

    :fire: feat - Select 기능 추가
    1. Svg 추가
    2. 애니메이션 효과 추가 (svg 돌리기, option 나타나기)

--- front/src/components/molecules/Select.tsx
+++ front/src/components/molecules/Select.tsx
@@ -1,5 +1,7 @@
 import { DisabledInput, SelectDiv } from '@components/atoms';
-import React, { ForwardedRef, forwardRef, useEffect, useMemo, useState } from 'react';
+import React, { ForwardedRef, forwardRef, useEffect, useState } from 'react';
+import Arrow from '@constants/svg/arrow.svg';
+import { useSpring, animated, config, useSpringRef, useChain } from '@react-spring/web';
 
 export interface SelectProps {
   theme?: 'gray' | 'orange';
@@ -18,24 +20,53 @@ const defaultData: dataTypes = {
   selectValue: true,
 };
 
+const selectAnimation = (open: boolean) => {
+  const { rotate } = useSpring({
+    from: { rotate: 0 },
+    to: { rotate: open ? 180 : 0 },
+    config: config.stiff,
+  });
+
+  return { rotate };
+};
+
+const optionAnimation = (open: boolean) => {
+  const opacityRef = useSpringRef();
+  const { opacity, cursor } = useSpring({
+    ref: opacityRef,
+    from: { opacity: 0, cursor: 'auto' },
+    to: {
+      opacity: open ? 1 : 0,
+      cursor: open ? 'pointer' : 'auto',
+    },
+    config: config.stiff,
+  });
+
+  const displayRef = useSpringRef();
+  const { display } = useSpring({
+    ref: displayRef,
+    from: { display: 'none' },
+    to: {
+      display: open ? 'block' : 'none',
+    },
+  });
+
+  useChain(open ? [displayRef, opacityRef] : [opacityRef, displayRef]);
+
+  return { opacity, display, cursor };
+};
+
 export const Select = forwardRef((props: SelectProps, ref: ForwardedRef<HTMLInputElement>) => {
   const { theme = 'gray', data = [], ...prop } = props;
 
   const [open, setOpen] = useState<boolean>(false);
   const [selectData, setSelectData] = useState<dataTypes>(defaultData);
 
-  const optionBorderColor = useMemo(() => (theme === 'gray' ? 'border-myGray' : 'border-myOrange'), [theme]);
-  const optionClassArr = [
-    'absolute',
-    'top-full',
-    'left-0',
-    'w-full',
-    'border-2',
-    'rounded-md',
-    `${optionBorderColor}`,
-    'z-10',
-    'inline-block',
-  ];
+  const optionBorderColor = theme === 'gray' ? 'border-myGray' : 'border-myOrange';
+  const svgColor = theme === 'gray' ? 'fill-myGray' : 'fill-myOrange';
+
+  const { opacity, display, cursor } = optionAnimation(open);
+  const { rotate } = selectAnimation(open);
 
   useEffect(() => {
     const select = data.find((key) => key.selectValue);
@@ -53,6 +84,8 @@ export const Select = forwardRef((props: SelectProps, ref: ForwardedRef<HTMLInpu
   };
 
   const onClickOption = (label: string, value: string) => () => {
+    if (!open) return;
+
     setSelectData({ label: label, value: value });
   };
 
@@ -63,22 +96,39 @@ export const Select = forwardRef((props: SelectProps, ref: ForwardedRef<HTMLInpu
 
   return (
     <div className="relative min-w-sm max-w-md" tabIndex={-1} onClick={onClickSelect} onBlur={onBlurSelect}>
-      <DisabledInput theme={theme} label={selectData.label} style={{ cursor: 'pointer' }} />
+      <DisabledInput theme={theme} label={selectData.label} style={{ cursor: 'pointer', paddingRight: '2.3rem' }} />
+      <animated.div
+        className="absolute top-2 right-2"
+        style={{ transform: rotate.to((rotate) => `rotateZ(${rotate}deg)`) }}
+      >
+        <Arrow className={svgColor} width="24" height="24" />
+      </animated.div>
       <input ref={ref} type="hidden" value={selectData.value} />
-      {open && (
-        <div className={optionClassArr.join(' ')}>
-          {data.map((key, index) => (
-            <SelectDiv
-              key={index}
-              label={key.label}
-              value={key.value}
-              selected={key.value === selectData.value}
-              index={isIndex(index)}
-              onClick={onClickOption}
-            />
-          ))}
-        </div>
-      )}
+      <animated.div
+        className={[
+          'absolute',
+          'top-full',
+          'left-0',
+          'w-full',
+          'border',
+          'rounded-md',
+          `${optionBorderColor}`,
+          'z-10',
+          'inline-block',
+        ].join(' ')}
+        style={{ opacity, display, cursor }}
+      >
+        {data.map((key, index) => (
+          <SelectDiv
+            key={index}
+            label={key.label}
+            value={key.value}
+            selected={key.value === selectData.value}
+            index={isIndex(index)}
+            onClick={onClickOption}
+          />
+        ))}
+      </animated.div>
     </div>
   );
 });
