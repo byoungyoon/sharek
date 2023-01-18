1c945368d865f0296e481335596fcd5af1a57542
byoungyoon <bur5698@naver.com>
2023-01-18 12:29:18 +0900

    :green_heart: fix - Select 주석 추가

--- front/src/components/molecules/Select.tsx
+++ front/src/components/molecules/Select.tsx
@@ -4,13 +4,33 @@ import Arrow from '@constants/svg/arrow.svg';
 import { useSpring, animated, config, useSpringRef, useChain } from '@react-spring/web';
 
 export interface SelectProps {
+  /**
+   * 테마 선택
+   * @default gray
+   */
   theme?: 'gray' | 'orange';
+
+  /**
+   * 데이터 배열
+   * @default []
+   */
   data?: Array<dataTypes>;
 }
 
 export type dataTypes = {
+  /**
+   * 보이는 값
+   */
   label: string;
+
+  /**
+   * 키 값
+   */
   value: string;
+
+  /**
+   * 선택된 값 여부 (많을 경우 첫번째 true 선택)
+   */
   selectValue?: boolean;
 };
 
@@ -20,6 +40,11 @@ const defaultData: dataTypes = {
   selectValue: true,
 };
 
+/**
+ * select 대한 애니메이션 (svg)
+ * @param open option 보이는 여부
+ * @returns useSpring option
+ */
 const selectAnimation = (open: boolean) => {
   const { rotate } = useSpring({
     from: { rotate: 0 },
@@ -30,6 +55,11 @@ const selectAnimation = (open: boolean) => {
   return { rotate };
 };
 
+/**
+ * option 대한 애니메이션
+ * @param open option 보이는 여부
+ * @returns useSpring option
+ */
 const optionAnimation = (open: boolean) => {
   const opacityRef = useSpringRef();
   const { opacity, cursor } = useSpring({
@@ -51,16 +81,19 @@ const optionAnimation = (open: boolean) => {
     },
   });
 
-  useChain(open ? [displayRef, opacityRef] : [opacityRef, displayRef]);
+  useChain(open ? [displayRef, opacityRef] : [opacityRef, displayRef]); // 애니메이션 동작 순서
 
   return { opacity, display, cursor };
 };
 
+/**
+ * Select Component (molecules)
+ */
 export const Select = forwardRef((props: SelectProps, ref: ForwardedRef<HTMLInputElement>) => {
   const { theme = 'gray', data = [], ...prop } = props;
 
-  const [open, setOpen] = useState<boolean>(false);
-  const [selectData, setSelectData] = useState<dataTypes>(defaultData);
+  const [open, setOpen] = useState<boolean>(false); // option 보이는 여부
+  const [selectData, setSelectData] = useState<dataTypes>(defaultData); // 선택한 데이터
 
   const optionBorderColor = theme === 'gray' ? 'border-myGray' : 'border-myOrange';
   const svgColor = theme === 'gray' ? 'fill-myGray' : 'fill-myOrange';
@@ -71,24 +104,40 @@ export const Select = forwardRef((props: SelectProps, ref: ForwardedRef<HTMLInpu
   useEffect(() => {
     const select = data.find((key) => key.selectValue);
 
-    if (select) setSelectData(select);
-    else data.unshift(defaultData);
+    if (select) setSelectData(select); // selectValue가 있다면 선택
+    else data.unshift(defaultData); // selectValue가 없다면 data 배열 맨 앞 추가
   }, [data]);
 
+  /**
+   * select 영역 선택 시 open toggle
+   */
   const onClickSelect = () => {
     setOpen(!open);
   };
 
+  /**
+   * select 영역 focus가 벗어날 시 open false
+   */
   const onBlurSelect = () => {
     setOpen(false);
   };
 
+  /**
+   * option 선택 action
+   * @param label 보이는 값
+   * @param value 키 값
+   */
   const onClickOption = (label: string, value: string) => () => {
     if (!open) return;
 
     setSelectData({ label: label, value: value });
   };
 
+  /**
+   * index first -> 'start', index last -> 'end'
+   * @param index option 순서 index
+   * @returns
+   */
   const isIndex = (index: number) => {
     if (index === 0) return 'start';
     else if (index === data.length - 1) return 'end';
