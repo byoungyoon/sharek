1e5fa3179ef311710c2370a50e7dab8b188b5bfc
byoungyoon <bur5698@naver.com>
2023-02-03 10:37:42 +0900

    :fire: feat - profile null => h2

--- back/src/main/java/co/kr/sharek/SharekApplication.java
+++ back/src/main/java/co/kr/sharek/SharekApplication.java
@@ -2,11 +2,15 @@ package co.kr.sharek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class SharekApplication {
 
   public static void main(String[] args) {
+    if(System.getProperty("spring.profiles.active") == null)
+      System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "h2");
+
     SpringApplication.run(SharekApplication.class, args);
   }
 
