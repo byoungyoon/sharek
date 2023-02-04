fc7687ecdf6a5e18ed41fbf464638536171cb9ab
forhyeonji <ghkdguswl3@gmail.com>
2023-02-04 15:43:44 +0900

    🍏 fix - Titles 코드 정리

--- front/src/components/molecules/Titles.tsx
+++ front/src/components/molecules/Titles.tsx
@@ -8,17 +8,13 @@ interface TitlesProps {
 
 export const Titles = (props: TitlesProps) => {
   const { label, fontWeight } = props;
-  const h1 = 'h1';
-  const h2 = 'h2';
-  const h3 = 'h3';
-  const h4 = 'h4';
 
   return (
     <div>
-      <Title fontWeight={fontWeight} label={label} fontStyle={h1} />
-      <Title fontWeight={fontWeight} label={label} fontStyle={h2} />
-      <Title fontWeight={fontWeight} label={label} fontStyle={h3} />
-      <Title fontWeight={fontWeight} label={label} fontStyle={h4} />
+      <Title fontWeight={fontWeight} label={label} fontStyle="h1" />
+      <Title fontWeight={fontWeight} label={label} fontStyle="h2" />
+      <Title fontWeight={fontWeight} label={label} fontStyle="h3" />
+      <Title fontWeight={fontWeight} label={label} fontStyle="h4" />
     </div>
   );
 };
