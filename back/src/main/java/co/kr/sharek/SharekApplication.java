a9230872b9488e514a5a458be7ffd1da0322efdf
kimseongmin0301 <hippo159753@gmail.com>
2023-02-12 19:35:16 +0900

    :fire: feat - page 1부터 시작하게 세팅

--- back/src/main/java/co/kr/sharek/SharekApplication.java
+++ back/src/main/java/co/kr/sharek/SharekApplication.java
@@ -2,7 +2,9 @@ package co.kr.sharek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @SpringBootApplication
 public class SharekApplication {
@@ -12,6 +14,13 @@ public class SharekApplication {
       System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "h2");
 
     SpringApplication.run(SharekApplication.class, args);
+  }
+
+  @Bean
+  public PageableHandlerMethodArgumentResolverCustomizer customize(){
+    return p -> {
+      p.setOneIndexedParameters(true); // page 1부터 시작
+    };
   }
 
 }
